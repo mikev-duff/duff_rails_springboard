1dc76c72b044fb28a51c0f9b4c10cf16d4156578
Mike Voytovich <mikev@duffresearch.com>
2012-04-16 15:37:35 -0700

    email unique test, make email an index in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416203745) do
+ActiveRecord::Schema.define(:version => 20120416223158) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20120416203745) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
   create_table "words", :force => true do |t|
     t.string   "name"
